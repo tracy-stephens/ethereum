4f223542432ba4447812c1cb00ae1a5b554a670c
tracy-stephens <tracy.stephens972@gmail.com>
2021-09-27 11:07:22 -0400

    reformat columns.py

--- columns.py
+++ columns.py
@@ -1,27 +1,66 @@
 COLUMNS = {
     "blocks": [
-        'number', 'miner', 'difficulty', 'total_difficulty', 'size', 'gas_limit',
-        'gas_used', 'timestamp', 'transaction_count', 'base_fee_per_gas'
+        'number',
+        'miner',
+        'difficulty',
+        'total_difficulty',
+        'size',
+        'gas_limit',
+        'gas_used',
+        'timestamp',
+        'transaction_count',
+        'base_fee_per_gas'
     ],
     "transactions": [
-        'hash', 'block_number', 'transaction_index', 'from_address', 'to_address', 'value', 'gas',
-        'gas_price', 'max_fee_per_gas', 'max_priority_fee_per_gas', 'transaction_type'
+        'hash',
+        'block_number',
+        'transaction_index',
+        'from_address',
+        'to_address',
+        'value',
+        'gas',
+        'gas_price',
+        'max_fee_per_gas',
+        'max_priority_fee_per_gas',
+        'transaction_type'
     ],
     "contracts": [
-        'address', 'function_sighashes', 'is_erc20', 'is_erc721', 'block_number'
+        'address',
+        'function_sighashes',
+        'is_erc20',
+        'is_erc721',
+        'block_number'
     ],
     "logs": [
-        'log_index', 'transaction_hash', 'block_number', 'address'
+        'log_index',
+        'transaction_hash',
+        'block_number',
+        'address'
     ],
     "receipts": [
-        'transaction_hash', 'block_number', 'cumulative_gas_used', 'gas_used', 'contract_address',
-        'status', 'effective_gas_price'
+        'transaction_hash',
+        'block_number',
+        'cumulative_gas_used',
+        'gas_used',
+        'contract_address',
+        'status',
+        'effective_gas_price'
     ],
-    "token_transfers" : [
-        'token_address', 'from_address', 'to_address', 'value', 'transaction_hash', 'log_index',
+    "token_transfers": [
+        'token_address',
+        'from_address',
+        'to_address',
+        'value',
+        'transaction_hash',
+        'log_index',
         'block_number'
     ],
     "tokens": [
-        'address', 'symbol', 'name', 'decimals', 'total_supply', 'block_number'
+        'address',
+        'symbol',
+        'name',
+        'decimals',
+        'total_supply',
+        'block_number'
     ]
 }
