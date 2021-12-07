2a88ca8d8ac1dc0b077bf8e6a21a512df6547360
tracy-stephens <tracy.stephens972@gmail.com>
2021-12-06 21:16:07 -0500

    new dashboard code with different views

--- web_rebuild/deploy_clean.py
+++ web_rebuild/deploy_clean.py
@@ -2,23 +2,12 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 import pandas as pd
-from dashboard import (
-    clean_dates,
-    clean_predicted,
-    surge_chart,
-    surge_index,
-    gas_hist,
-    pct_tip_chart,
-    minute_of_day_chart,
-    minute_of_hour_chart,
-    pct_smart_contract_chart
-)
+from dashboard import *
 from datetime import datetime
 import time
 
 # fix streamlit layout
 st.set_page_config(layout='wide')
-
 st_autorefresh(interval=60000)
 
 TZ = "US/Eastern"
@@ -43,6 +32,11 @@ data = clean_dates(data_raw, tz=TZ)
 # charts
 surge_cht = surge_chart(clean_predicted(data["predicted"]))
 current_cht = gas_hist(data["predicted"])
+minute_day_cht = minute_of_day_chart(data["blocks"])
+minute_hour_cht = minute_of_hour_chart(data["blocks"])
+contract_cht = pct_smart_contract_chart(data["transactions"])
+tip_cht = pct_tip_chart(data["transactions"])
+
 
 # fix overhead padding
 padding = 1
@@ -80,56 +74,44 @@ st.markdown(
     unsafe_allow_html=True)
 
 # core website content
-last_pred = str(round(data["predicted"].iloc[-1][1]/1000000000))
-low = 'rgba(230,242,231)'
-med = 'rgba(255,255,220)'
-hi = 'rgba(255,231,233)'
-
-# get surge color cutoffs
-thresh_df = surge_index(data["predicted"])
-thresh_df.columns = [['Realized Gas Price', 'Predicted Gas Price']]
-thresh_df = thresh_df.unstack().reset_index()
-thresh_df.columns = ['Name', 'Time', 'Value']
-thresh = thresh_df['Value'].iloc[-1]
-
-if thresh <= 1.5:
-    surge_color = low
-    recommendation = "Stoplight is Green: We recommend executing your transaction now, transaction fees are currently low."
-elif thresh > 1.5 and thresh <= 2.0:
-    surge_color = med
-    recommendation = "Stoplight is Yellow: Transaction fees are currently slightly higher than normal. You could do better, but if you must execute your transaction, it is not a bad time to do so."
-elif thresh > 2.0:
-    surge_color = hi
-    recommendation = "Stoplight is Red: We recommend waiting to execute your transaction, transaction fees are currently high."
-
-# define stoplight
-circle = '<style>.circle {width: 200px; height: 200px; line-height: 200px; margin-left: 20px; border-radius: 50%; font-size: 20px; color: #000; text-align: center; background:' + surge_color + '}</style>' + '<div class="circle">' + last_pred + 'B gwei</div>'
+last_pred, recommendation, stoplight_markdown = stoplight(data["predicted"])
 
 # make sidebar
 st.sidebar.title('Ethereum Stoplight')
 #st.sidebar.header('Applied Machine Learning')
 st.sidebar.subheader('Ethereum Transaction Price Estimator')
+
+menu = st.sidebar.radio(
+    "View",
+    ("Stoplight", "Time Patterns", "Recent Transactions"),
+)
+
 st.sidebar.markdown('Current ethereum gas price estimators either use simple historical price trends or settle for limited supply-side (miner) data to guess a potentially successful transaction fee.')
 st.sidebar.markdown('Ethereum Stoplight applies machine learning to gigabytes and gigabytes of historical transaction-level and block-level smart contracts activity data to accurately predict the current required fee to successfully get your transaction added to the next block.')
 
-col1, col2 = st.columns([1,2])
+if menu == 'Stoplight':
+    col1, col2 = st.columns([1,2])
+    with col1:
+        st.markdown('**Current predicted gas price:**')
+        st.markdown(stoplight_markdown , unsafe_allow_html = True)
+        st.markdown(recommendation)
 
-
-with col1:
-    st.markdown('**Current predicted gas price:**')
-    st.markdown(circle, unsafe_allow_html = True)
-    st.markdown(recommendation)
+    with col2:
+        st.markdown('**Surge Index**')
+        st.plotly_chart(surge_cht)
+    
     st.plotly_chart(current_cht)
-    st.plotly_chart(minute_of_day_chart(data["blocks"]))
-    st.plotly_chart(minute_of_hour_chart(data["blocks"]))
-    st.plotly_chart(pct_smart_contract_chart(data["transactions"]))
-    st.plotly_chart(pct_tip_chart(data["transactions"]))
-    
-with col2:
-    st.markdown('**Surge Index**')
-    st.plotly_chart(surge_cht)
-    
-    
+        
+if menu == 'Time Patterns':
+    st.plotly_chart(minute_day_cht)
+    st.plotly_chart(minute_hour_cht)
+
+if menu == 'Recent Transactions':
+    st.plotly_chart(contract_cht)
+    st.plotly_chart(tip_cht)
+
+ 
+
 
 
 
